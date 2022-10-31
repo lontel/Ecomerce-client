3e5e3b31f04ca6c02607582f36c2828f9cb9bc24
lontel <lonte.leon@gmail.com>
2022-10-31 23:50:02 +0100

    add login/register form

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import MainLayout from 'hoc/mainLayout'
 import Header from './components/navigation/header'
 import Footer from './components/navigation/footer'
 import Home from './components/home'
+import RegisterLogin from 'components/auth'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <MainLayout>
         <Routes>
+          <Route path="/sign_in" element={<RegisterLogin />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </MainLayout>
