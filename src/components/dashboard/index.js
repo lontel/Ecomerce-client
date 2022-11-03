be60e65d9f1b29f21728ce9f62896dc10c856eb2
lontel <lonte.leon@gmail.com>
2022-11-03 11:45:40 +0100

    add dashboard route and dashboard layout

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import DashboardLayout from "hoc/dashboardLayout"
+
+const Dashboard = () => {
+    return (
+        <>
+            <DashboardLayout title='Overview'>
+                kudsgiugiudgv
+            </DashboardLayout>
+        </>
+    )
+}
+
+export default Dashboard
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import MainLayout from 'hoc/mainLayout'
 
+import { useDispatch, useSelector } from 'react-redux'
+import { userIsAuth, userSignOut } from 'store/actions/user.actions'
+
 import Header from './components/navigation/header'
 import Footer from './components/navigation/footer'
 import Home from './components/home'
 import RegisterLogin from 'components/auth'
+import Dashboard from './components/dashboard'
 
-import { useDispatch, useSelector } from 'react-redux'
-import { userIsAuth, userSignOut } from 'store/actions/user.actions'
 
 const App = (props) => {
   const users = useSelector(state => state.users)
@@ -30,6 +32,7 @@ const App = (props) => {
       />
       <MainLayout>
         <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/sign_in" element={<RegisterLogin />} />
           <Route path="/" element={<Home />} />
         </Routes>
